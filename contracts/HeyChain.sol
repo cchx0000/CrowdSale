6f3f801f6ab5fd1147a966689fb25c7ae2194ead
cchx0000 <Chenxi_Cai@live.com>
2018-05-29 22:18:24 +0800

    added files

---
+++ contracts/HeyChain.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.11;
+
+import '../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract HeyChain is MintableToken {
+  string public name = "Hey Chain";
+  string public symbol = "HEY";
+  uint256 public decimals = 18;
+}
